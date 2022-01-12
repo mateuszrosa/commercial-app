fcd61b2c59e9b498e469ec23e1a9e0a9653dce05
mateuszrosa <mateusz.rosa@gmail.com>
2022-01-12 16:41:00 +0100

    form for logging

--- src/views/Userpage/Userpage.js
+++ src/views/Userpage/Userpage.js
@@ -3,12 +3,17 @@ import styles from './Userpage.module.scss';
 const Userpage = () => {
     return ( 
         <div className={styles.userpage}>
+            <div className={styles.container}>
             <h2>log in</h2>
             <form action="">
-                <input type="text"/>
-                <input type="password" name="" id=""/>
+                <label htmlFor="login">Login</label>
+                <input type="text" name="login" id="login" placeholder='Write your login'/>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" placeholder='Write your password'/>
                 <input type="submit" value="log in" />
+                <input type="button" value="register" />
             </form>
+            </div>
         </div>
      );
 }
