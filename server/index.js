60b325fbd08ce523e50c80de2e4f5b6ed4d46890
mateuszrosa <mateusz.rosa@gmail.com>
2022-01-13 12:10:06 +0100

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-import express from 'express';
+const express = require('express');
+const {MongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+const connectionString = process.env.NODE_DATABASE;
+
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+  })
+  .catch(error => console.error(error))
 
 const app = express();
 const port = 3500;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}`);
