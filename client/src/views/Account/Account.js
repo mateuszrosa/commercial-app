fa09146838f46b19032483b263455a02ef0b8f9c
mateuszrosa <mateusz.rosa@gmail.com>
2022-04-05 21:58:21 +0200

    added two panels to account component

--- client/src/views/Account/Account.js
+++ client/src/views/Account/Account.js
@@ -3,7 +3,8 @@ import styles from './Account.module.scss';
 import { Redirect } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux';
 import {editUser} from '../../actions';
-import { Button, Grid, Typography} from '@mui/material';
+import {Box, Tab, Button, Grid, Typography} from '@mui/material';
+import {TabContext, TabList, TabPanel} from '@mui/lab';
 import {useForm,FormProvider} from 'react-hook-form';
 import {FormInput} from '../../components/CheckoutForm/CustomTextField';
 
@@ -14,7 +15,13 @@ export const Account = () => {
   const {user} = useSelector(({ user }) => ({
     user
   }));
+
+  const [value, setValue] = useState('1');
   const [isEdited, setIsEdited] = useState(false);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const submit = (data) => {
     dispatch(editUser(data, user.login));
@@ -28,24 +35,37 @@ export const Account = () => {
 };
 
   return (
-    <div className={styles.container}>
-        <Typography variant="h6" gutterBottom>Edit account</Typography>
-      <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit((data) => submit(data))}>
-          <Grid container spacing={3}>
-            <FormInput name="firstName" label="First name" value={user.firstName}/>
-            <FormInput name="lastName" label="Last name" value={user.lastName} />
-            <FormInput name="address1" label="Address" value={user.address1} />
-            <FormInput name="email" label="Email" value={user.email} />
-            <FormInput name="City" label="City" value={user.city} />
-            <FormInput name="zip" label="ZIP/Postal Code" value={user.zip} />
-          </Grid>
-          <br />
-          <div style={{display: 'flex', justifyContent: 'space-between'}}>
-            <Button type="submit" variant="contained" color="primary">Save</Button>
-          </div>
-        </form>
-      </FormProvider>
+  <Box sx={{ width: '100%', typography: 'body1', marginTop: '90px' }}>
+  <TabContext value={value}>
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Change address" value="1" />
+            <Tab label="Orders" value="2" />
+            <Tab label="Item Three" value="3" />
+          </TabList>
+    </Box>
+    <TabPanel value="1">
+    <Typography variant="h6" gutterBottom></Typography>
+  <FormProvider {...methods}>
+    <form onSubmit={methods.handleSubmit((data) => submit(data))}>
+      <Grid container spacing={3}>
+        <FormInput name="firstName" label="First name" value={user.firstName}/>
+        <FormInput name="lastName" label="Last name" value={user.lastName} />
+        <FormInput name="address1" label="Address" value={user.address1} />
+        <FormInput name="email" label="Email" value={user.email} />
+        <FormInput name="City" label="City" value={user.city} />
+        <FormInput name="zip" label="ZIP/Postal Code" value={user.zip} />
+      </Grid>
+      <br />
+      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <Button type="submit" variant="contained" color="primary">Save</Button>
       </div>
+    </form>
+  </FormProvider>
+    </TabPanel>
+    <TabPanel value="2">Item Two</TabPanel>
+    <TabPanel value="3">Item Three</TabPanel>
+  </TabContext>
+</Box>
   );
 }
